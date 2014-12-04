98322f3e53f5b011fa1fd59e365d64507c7c1710
Juan Pablo Cuadro <jp.cuadro@gmail.com>
2014-12-04 14:22:05 +0100

    Add fine grain progress updating

--- wscript_BER_CURVES_RS.m
+++ wscript_BER_CURVES_RS.m
@@ -4,7 +4,7 @@
 close all
 clear all
 
-N_SYMBOLS = 4 * 5 * 1.504 * 1e4;
+N_SYMBOLS = 4 * 5 * 1.504 * 1e5;
 LABEL_SCENARIOS = {'Theoretical',...
     'Simulated - no FEC',...
     'Simulated - Conv Hard Deco - R = 1/2',...
@@ -30,6 +30,8 @@ BER(1,:) = 0.5*erfc(sqrt(10.^(EbN0_dB/10)));
 
 tic
 progressbar(0);
+loop_counter = 0;
+loop_total = 6 * length(EbN0_dB);
 
 for i = 1:length(EbN0_dB)
     
@@ -38,24 +40,33 @@ for i = 1:length(EbN0_dB)
     % No FEC
     obj.setScenario('nofec');
     BER(2,i) = obj.simulate(EbN0_dB(i));
+    loop_counter = loop_counter + 1;
+    progressbar(loop_counter/loop_total);
     % Hard dec
     obj.setScenario('convh');
     BER(3,i) = obj.simulate(EbN0_dB(i));
+    loop_counter = loop_counter + 1;
+    progressbar(loop_counter/loop_total);
     % Soft dec
     obj.setScenario('convs');
     BER(4,i) = obj.simulate(EbN0_dB(i));
+    loop_counter = loop_counter + 1;
+    progressbar(loop_counter/loop_total);
     % Puncturing!
     obj.setScenario('convp');
     BER(5,i) = obj.simulate(EbN0_dB(i));
+    loop_counter = loop_counter + 1;
+    progressbar(loop_counter/loop_total);
     % Puncturing and RS!
     obj.setScenario('rsit0');
     BER(6,i) = obj.simulate(EbN0_dB(i));
+    loop_counter = loop_counter + 1;
+    progressbar(loop_counter/loop_total);
     % Interleaving
     obj.setScenario('rsit1');
     BER(7,i) = obj.simulate(EbN0_dB(i));
-    
-    % Update Progress
-    progressbar(i/length(EbN0_dB));
+    loop_counter = loop_counter + 1;
+    progressbar(loop_counter/loop_total);
     
 end
 toc
